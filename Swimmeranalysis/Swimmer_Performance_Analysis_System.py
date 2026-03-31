f02cefd11b4b89c6d9b4e8235c1242b2576ecda8
Hannah Blank <hannah2010b@gmail.com>
2026-03-31 21:11:33 +0200

    .

--- Swimmeranalysis/Swimmer_Performance_Analysis_System.py
+++ Swimmeranalysis/Swimmer_Performance_Analysis_System.py
@@ -1,6 +1,5 @@
-fn= "Darius-13-100m-Fly.txt"
-swimmer, age_group, distance, stroke = fn.removesuffix(".txt").split("-")
-print(swimmer)
-print(age_group)
-print(distance)
-print(stroke)
+FN = "Darius-13-100m-Fly.txt"
+FOLDER = "swimdata/"
+with open(FOLDER + FN) as df:
+    data = df.readlines()
+times = data[0].strip().split(",")
