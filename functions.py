a750633b99baf11b2ecf42e82815633040109b5b
Hannah Blank <hannah2010b@gmail.com>
2026-07-10 15:11:23 +0200

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,14 @@
+
+def happy_birthday(name, age):
+    print(f"Happy birthday to {name}!")
+    print(f"You are {age} years old!")
+    print(f"Happy birthday to you!")
+    print()
+
+happy_birthday("Bro","20")
+
+def display_invoice(username, amount, due_date):
+    print(f"Hello {username}")
+    print(f"Your bill of €{amount:.2f} is due on: {due_date}")
+
+display_invoice("Bro", 100, "2024-07-01")
